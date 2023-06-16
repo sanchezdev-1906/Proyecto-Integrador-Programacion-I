14a5d135abf37f46fbd756a2e8e0b041e0e48013
Joel Sanchez <joelalejandro.development@gmail.com>
2023-06-16 19:05:57 -0400

    parser agregado

---
+++ Calculadora.Funciones.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Integrador_Programacion_I
+{
+    partial class Calculadora
+    {
+        
+        Dictionary<string, HashSet<int>> ConjuntosElementos = new Dictionary<string, HashSet<int>>();
+        List<int> CalcularConjuntos(string cadena)
+        {
+            cadena = cadena.Replace(" ", "");
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            Duplicados(cadena);
+            int inicioParentesis = cadena.IndexOfAny(new char[] { '(', ')' });
+            if (inicioParentesis != -1 && cadena[inicioParentesis] == ')')
+                throw new Exception("Error de sintaxis )");
+            if (inicioParentesis != -1 && cadena[inicioParentesis] == '(' && EncontrarUltimoParentesis(cadena, inicioParentesis) == -1)
+                throw new Exception("Error de sintaxis ()");
+            if (cadena[0] == '(' && EncontrarUltimoParentesis(cadena, inicioParentesis) == cadena.Length - 1)
+                cadena = cadena.Substring(1, cadena.Length - 2);
+            if (cadena.Length == 1) return ConjuntosElementos[cadena];
+            else
+            {
+                List<List<int>> Conjuntos = new List<List<int>>();
+                List<char> Operadores = new List<char>();
+                string[] expressions = ExtraerExpresiones(cadena);
+                foreach (var expression in expressions)
+                {
+                    Conjuntos.Add(CalcularConjuntos(expression));
+                    cadena = cadena.Remove(0, expression.Length);
+                    if (cadena.Length != 0)
+                    {
+                        Operadores.Add(cadena[0]);
+                        cadena = cadena.Remove(0, 1);
+                    }
+                }
+                // Prioridad de operadores
+                while (Conjuntos.Count() != 1)
+                {
+                    Conjuntos[0] = Operar(Conjuntos[0], Conjuntos[1], Operadores[0]);
+                    Conjuntos.RemoveAt(1);
+                }
+                return Conjuntos[0];
+            }
+        }
+        /// <summary>
+        ///  Lanza una excepcion cuando existen dos simbolos iguales que no son parentesis
+        /// </summary>
+        void Duplicados(string cadena)
+        {
+            for (int i = 0; i < cadena.Length - 1; i++)
+            {
+                if (cadena[i] == cadena[i + 1] && cadena[i] != ')' && cadena[i] != '(')
+                {
+                    throw new Exception("Letras Duplicadas");
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Realiza las operaciones de conjuntos entre A y B segun el operador
+        /// </summary>
+        HashSet<int> Operar(HashSet<int> A, HashSet<int> B, char operador)
+        {
+            switch (operador)
+            {
+                case 'U': return Enumerable.Union(A, B).ToHashSet();
+                case 'I': return Enumerable.Intersect(A, B).ToHashSet();
+                case 'D': return Enumerable.Except(A, B).ToHashSet();
+                case 'S': return Enumerable.Except(Enumerable.Union(A, B), Enumerable.Intersect(A, B)).ToHashSet();
+                default: return Enumerable.Union(A, B).ToHashSet();
+            }
+        }
+
+        /// <summary>
+        ///  Crea un List con cada expresion del nivel 0.
+        ///  Ej:
+        /// (AUB)-CU(A-C) => (AUB), C, (A-C)
+        /// </summary>
+        string[] ExtraerExpresiones(string cadena)
+        {
+            List<string> expressions = new List<string>();
+            while (cadena.Length != 0)
+            {
+                int ia = cadena.IndexOfAny(new char[] { 'U', 'I', 'D', 'S' });
+                if (ia == 0)
+                {
+                    cadena = cadena.Remove(0, 1);
+                    continue;
+                }
+
+                if (cadena[0] == '(')
+                {
+                    expressions.Add(cadena.Substring(0, EncontrarUltimoParentesis(cadena, 0) + 1));
+                    cadena = cadena.Remove(0, EncontrarUltimoParentesis(cadena, 0) + 1);
+                }
+                else
+                {
+                    if (ia == -1)
+                    {
+                        expressions.Add(cadena.Substring(0, cadena.Length));
+                        cadena = cadena.Remove(0, cadena.Length);
+                    }
+                    else
+                    {
+                        expressions.Add(cadena.Substring(0, ia));
+                        cadena = cadena.Remove(0, ia);
+                    }
+                }
+            }
+            return expressions.ToArray<string>();
+        }
+        /// <summary>
+        ///  Dado el indice de un parentesis de apertura, busca el parentesis de cierre y devuelve su indice.
+        /// </summary>
+        int EncontrarUltimoParentesis(string cadena, int inicioParentesis)
+        {
+            int aux = 0;
+            int index;
+            for (index = 0; index < cadena.Length; index++)
+            {
+                if (cadena[index] == '(')
+                {
+                    aux++;
+                }
+                if (cadena[index] == ')')
+                {
+                    aux--;
+                    if (aux == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            return index == cadena.Length ? -1 : index;
+        }
+
+    }
+}
--- Calculadora.cs
+++ Calculadora.cs
@@ -2,9 +2,14 @@ namespace Proyecto_Integrador_Programacion_I
 {
     public partial class Calculadora : Form
     {
+        
         public Calculadora()
         {
             InitializeComponent();
+            ConjuntosElementos.Add("A", new HashSet<int>());
+            ConjuntosElementos.Add("B", new HashSet<int>());
+            ConjuntosElementos.Add("C", new HashSet<int>());
+
         }
     }
 }
