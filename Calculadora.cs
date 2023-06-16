5f8592569ec0ec7174748fd6e9df53c93bc4d45c
Joel Sanchez <joelalejandro.development@gmail.com>
2023-06-16 16:50:44 -0400

    primer commit

---
+++ Calculadora.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Integrador_Programacion_I
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+    }
+}
