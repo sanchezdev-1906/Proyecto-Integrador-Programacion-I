966c28d026be93eb7ce0157d56c681a0d1b28b76
Joel Sanchez <joelalejandro.development@gmail.com>
2023-06-20 18:57:40 -0400

    CElementos modificados

--- Calculadora.Eventos.cs
+++ Calculadora.Eventos.cs
@@ -9,11 +9,11 @@ namespace Proyecto_Integrador_Programacion_I
 {
     partial class Calculadora
     {
-        HashSet<int> CrearConjunto(string expresion)
+        static public HashSet<int> CrearConjunto(string expresion)
         {
             int inicio, fin;
             HashSet<int> conjunto = new HashSet<int>();
-            if (Regex.Match(expresion, @"^[(\[]-?\d\d*,-?\d\d*[)\]]$").Success)
+            if (Regex.Match(expresion, @"^[(\[]-?\d+,-?\d+[)\]]$").Success)
             {
                 int[] lim = Array.ConvertAll(
                     expresion
@@ -25,18 +25,22 @@ namespace Proyecto_Integrador_Programacion_I
                     , int.Parse);
 
                 inicio = expresion[0] == '[' ? lim[0] : lim[0] + 1;
-                fin = expresion[expresion.Length - 1] == ']' ? lim[1] : lim[1] + 1;
+                fin = expresion[expresion.Length - 1] == ']' ? lim[1] : lim[1] - 1;
 
                 for (int i = inicio; i <= fin; i++)
                 {
                     conjunto.Add(i);
                 }
-
+                
             }
-            else
+            else if(Regex.Match(expresion, @"^-?\d+(?:,-?\d+)*$").Success)
             {
                 int[] lim = Array.ConvertAll(expresion.Split(","), int.Parse);
-                Array.Sort(lim);
+                conjunto = new HashSet<int>(lim);
+            }
+            else if (Regex.Match(expresion, @"^-?\d+(?:<|≤)x+(?:<|≤)+-?\d+$").Success)
+            {
+                int[] lim = Array.ConvertAll(expresion.Split(), int.Parse);
                 conjunto = new HashSet<int>(lim);
             }
             return conjunto;
