8d70c5131b4a3bb25534c3834fcf4fa5e7cc0aba
Joel Sanchez <joelalejandro.development@gmail.com>
2023-06-20 18:57:12 -0400

    Centrar la ventana secundaria al cambiar el tamÃaño

--- Calculadora.cs
+++ Calculadora.cs
@@ -6,19 +6,41 @@ namespace Proyecto_Integrador_Programacion_I
         {
             InitializeComponent();
         }
-        private void MostrarVentana(Form opcion)
+        private void MostrarVentana(Form newForm)
         {
             if (pnlPrincipal.Controls.Count > 0)
                 pnlPrincipal.Controls.Clear();
-            opcion.TopLevel = false;
-            opcion.Dock = DockStyle.Fill;
-            pnlPrincipal.Controls.Add(opcion);
-            opcion.Show();
+            newForm.TopLevel = false;
+            pnlPrincipal.Controls.Add(newForm);
+            CentrarElementoAlPanel();
+            newForm.Show();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             MostrarVentana(new OpConjuntos());
+            button1.ForeColor = Color.FromArgb(255, 233, 205, 93);
+        }
+
+        private void Calculadora_SizeChanged(object sender, EventArgs e)
+        {
+
+            CentrarElementoAlPanel();
+        }
+        private void CentrarElementoAlPanel()
+        {
+
+            int x;
+            if (pnlPrincipal.Controls.Count > 0)
+            {
+                Control hijo = pnlPrincipal.Controls[0];
+
+                //un poco de matematicas, restando los anchos y dividiendo entre 2
+                x = (pnlPrincipal.Width / 2) - (hijo.Width / 2);
+
+                //asignamos la nueva ubicación
+                hijo.Location = new Point(x, hijo.Location.Y);
+            }
         }
     }
 }
