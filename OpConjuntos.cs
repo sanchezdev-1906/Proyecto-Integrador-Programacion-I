cec60b2c0b9697d12e8b03e85dc3e3b3bc1484c3
SergioV8 <sergiovargasumacacho@gmail.com>
2023-06-20 22:11:58 -0400

    Funcionalidad del los botones

--- OpConjuntos.cs
+++ OpConjuntos.cs
@@ -15,6 +15,74 @@ namespace Proyecto_Integrador_Programacion_I
         public OpConjuntos()
         {
             InitializeComponent();
+            rBtnConjs2.Checked = true;
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void rBtnConjs3_CheckedChanged(object sender, EventArgs e)
+        {
+            lblConjC.Visible = true;
+            txtConjC.Visible = true;
+        }
+
+        private void rBtnConjs2_CheckedChanged(object sender, EventArgs e)
+        {
+            lblConjC.Visible = false;
+            txtConjC.Visible = false;
+        }
+
+        private void BtnInterChart_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "∩";
+        }
+
+        private void BtnUnionChart_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "∪";
+        }
+
+        private void BtnDiferenciaChart_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "-";
+        }
+
+        private void BtnDifSimeChart_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "∆";
+        }
+
+        private void BtnCompleChart_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "ᶜ";
+        }
+
+        private void BtnVacioChart_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "∅";
+        }
+
+        private void BtnAchart_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "A";
+        }
+
+        private void BtnBchar_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "B";
+        }
+
+        private void BtnCchar_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "C";
+        }
+
+        private void BtnUchar_Click(object sender, EventArgs e)
+        {
+            txtOper.Text += "U";
         }
     }
 }
